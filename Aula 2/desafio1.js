a444d501b0adc127dafaa45d5397bade8d4c6caa
Leticia Pena <leticiapenavila@gmail.com>
2024-02-03 14:55:22 -0300

    feat: alimentando os desafios da aula 2

---
+++ Aula 2/desafio1.js
@@ -0,0 +1,8 @@
+const numero1 = 7
+const numero2 = 5
+
+const resultado = [numero1 + numero2, numero1 - numero2, numero1 * numero2, numero1 / numero2 ]
+
+console.log(resultado)
+
+
---
+++ Aula 2/desafio3.js
@@ -0,0 +1,7 @@
+const matriz = [
+    [2, 4],
+    [6, 8]
+    ]
+  
+    console.log((matriz[0][0] * matriz[1][1]) - (matriz[0][1] * matriz [1][0]))
+
